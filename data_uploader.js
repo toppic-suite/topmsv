ede376b44571c6015094cc9b961a12261e2af7d2
Jiang, Tianze <jiangtia@iu.edu>
2019-09-20 15:13:21 -0400

    change upload input style

--- data_uploader.js
+++ data_uploader.js
@@ -1,37 +1,44 @@
-// var http = require('http');
-// var formidable = require('formidable');
 var fs = require('fs');
 var nodemailer = require('nodemailer');
 const sqlite3 = require('sqlite3').verbose();
-// var url = require('url');
 var bodyParser = require('body-parser');
 var multer  = require('multer');
 var express = require('express');
 var app = express();
+var upload = multer({ dest: 'tmp/' });
+// var url = require('url');
+// var http = require('http');
+// var formidable = require('formidable');
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({ dest: '/tmp/'}).array('filetoupload'));
+// app.use(multer({ dest: '/tmp/'}).array('filetoupload'));
 
 app.get('/', function (req, res) {
     res.sendFile( __dirname + "/public/" + "index.html" );
 })
 
-app.post('/fileupload', function (req, res) {
+app.post('/fileupload',upload.single('filetoupload'),  function (req, res, next) {
+    // console.log(req.file);
+    const file = req.file;
+    if (!file) {
+        const error = new Error('Please upload a file');
+        error.httpStatusCode = 400;
+        return next(error)
+    }
     var des_path = __dirname + "/data/"+ req.body.projectname + '/';
-    var des_file = __dirname + "/data/"+ req.body.projectname + '/' + req.files[0].originalname;
-    var fname = req.files[0].originalname;
+    var des_file = __dirname + "/data/"+ req.body.projectname + '/' + req.file.originalname;
+    var fname = req.file.originalname;
     var pname = req.body.projectname;
     var emailadd = req.body.emailaddress;
-
+    console.log(des_path);
     // Generate new path for file
     if (!fs.existsSync(des_path)) {
         console.log('The project path does not exist.');
         fs.mkdirSync(des_path);
         console.log('Project path created!')
     }
-    // console.log(des_file);
-    fs.readFile( req.files[0].path, function (err, data) {
+    fs.readFile( req.file.path, function (err, data) {
         fs.writeFile(des_file, data, function (err) {
             if( err ){
                 console.log( err );
@@ -80,7 +87,7 @@ app.post('/fileupload', function (req, res) {
                 })
                 response = {
                     message:'File uploaded successfully',
-                    filename:req.files[0].originalname
+                    filename:req.file.originalname
                 };
             }
             console.log( response );
@@ -129,7 +136,6 @@ var server = app.listen(8080, function () {
 
 })
 
-
 function makeid(length) {
     var result           = '';
     var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_';
@@ -139,7 +145,6 @@ function makeid(length) {
     }
     return result;
 }
-
 
 function getFilePath(db, id, callback) {
     let sql = `SELECT file_path AS path
@@ -188,8 +193,6 @@ function ifExists(db, base64_code, callback) {
         }
 
     });
-    // close the database connection
-    // db.close();
 }
 
 function insertRow(db, base64_code, file_name, file_path) {
@@ -204,9 +207,6 @@ function insertRow(db, base64_code, file_name, file_path) {
         // get the last insert id
         console.log(`A row has been inserted with rowid ${this.lastID}`);
     });
-
-    // close the database connection
-    // db.close();
 }
 
 let db = new sqlite3.Database('./db/fileDB.db', (err) => {
@@ -216,117 +216,38 @@ let db = new sqlite3.Database('./db/fileDB.db', (err) => {
     console.log('Connected to the fileDB.db database.');
 });
 
+// mailtrap for testing
+// var transport = nodemailer.createTransport({
+//     host: "smtp.mailtrap.io",
+//     port: 2525,
+//     auth: {
+//         user: "9540ab1d87da99",
+//         pass: "133e06c63858da"
+//     }
+// });
+
 var transport = nodemailer.createTransport({
-    host: "smtp.mailtrap.io",
-    port: 2525,
+    host: "smtp-mail.outlook.com", // hostname
+    secureConnection: false, // TLS requires secureConnection to be false
+    port: 587, // port for secure SMTP
+    tls: {
+        ciphers:'SSLv3'
+    },
     auth: {
-        user: "9540ab1d87da99",
-        pass: "133e06c63858da"
+        user: 'datalink_sender@outlook.com',
+        pass: 'iupuiSOICWK316'
     }
 });
 
 const message = {
-    from: '9f8d9dbe08-22ea50@inbox.mailtrap.io', // Sender address
-    to: 'default@email.com',         // List of recipients
+    from: 'datalink_sender@outlook.com', // Sender address
+    to: 'jiangtianze@gmail.com',         // List of recipients
     subject: 'Default Subject', // Subject line
     text: 'Default text' // Plain text body
 };
 
-// http.createServer(function (req, res) {
-//     if (req.url == '/fileupload' && req.method.toLowerCase() == 'post') {
-//         var form = new formidable.IncomingForm();
-//         form.parse(req, function (err, fields, files) {
-//             var oldpath = files.filetoupload.path;
-//             var newfile = './data/' + fields.projectname + '/' + files.filetoupload.name;
-//             var newpath = './data/' + fields.projectname + '/';
-//             let fname = files.filetoupload.name;
-//             var pname = fields.projectname;
-//             var emailadd = fields.emailaddress;
-//
-//             if (!fs.existsSync(newpath)) {
-//                 console.log('The project path does not exist.');
-//                 fs.mkdirSync(newpath);
-//                 console.log('Project path created!')
-//             }
-//
-//             fs.rename(oldpath, newfile, function (err) {
-//                 if (err) throw err;
-//
-//                 var adr =  'http://localhost:8080/data?id=';
-//
-//                 res.write('<h1>File uploaded successfully!</h1>');
-//                 res.write('<h2>Project Name: </h2>');
-//                 res.write(pname);
-//                 res.write('<h2>File Path: </h2>');
-//                 res.write(newfile);
-//                 let id = makeid(11);
-//                 // let db = new sqlite3.Database('./db/fileDB.db');
-//                 ifExists(db, id, function (err, result) {
-//                     if(err) {
-//                         console.log(err);
-//                     }
-//                     else {
-//                         while (true) {
-//                             console.log(result);
-//                             if(!result) {
-//                                 insertRow(db, id, fname, newfile);
-//                                 message.text = adr + id;
-//                                 message.subject = pname + ': '+ files.filetoupload.name;
-//                                 message.to = emailadd;
-//                                 transport.sendMail(message, function(err, info) {
-//                                     if (err) {
-//                                         console.log(err)
-//                                     } else {
-//                                         console.log(info);
-//                                     }
-//                                 });
-//                                 res.end();
-//                                 break;
-//                             }
-//                         }
-//                     }
-//
-//                 })
-//             });
-//         });
-//
-//     } else if (url.parse(req.url, true).pathname =='/data'){
-//         let q = url.parse(req.url, true);
-//         let id = q.query.id;
-//         // console.log(id);
-//         getFilePath(db, id, function (err, result) {
-//             if (err) {
-//                 console.log(err);
-//             }
-//             else {
-//                 console.log(result);
-//                 fs.readFile(result, function(err, data) {
-//                     if (err) {
-//                         res.writeHead(404, {'Content-Type': 'text/html'});
-//                         return res.end("404 Not Found");
-//                     }
-//                     res.writeHead(200, {'Content-Type': 'text/html'});
-//                     res.write(data);
-//                     return res.end();
-//                 });
-//             }
-//         });
-//     } else {
-//         res.writeHead(200, {'Content-Type': 'text/html'});
-//         res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-//         res.write('<h1> Data uploader </h1>');
-//         res.write('<p>Please type in your project name and choose your data file.</p>');
-//
-//         res.write('<label id = "projectName">Project Name: </label>');
-//         res.write('<input id = "projectName" type="text" name="projectname" required><br>');
-//         res.write('<label id = "Email">Email Address: </label>');
-//         res.write('<input id = "Email" type="text" name="emailaddress" required><br>');
-//         res.write('<label id = "fileName">Data file: </label>');
-//         res.write('<input id = "fileName" type="file" name="filetoupload" required><br>');
-//
-//
-//         res.write('<br><input type="submit">');
-//         res.write('</form>');
-//         return res.end();
-//     }
-// }).listen(8080);
+process.on('SIGINT', () => {
+    console.log('Close database and server!')
+    db.close();
+    server.close();
+});
