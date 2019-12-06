a8323053024bf1992dc5c94f586efecea7941361
jiangtia <jiangtia@iu.edu>
2019-12-06 18:11:31 -0500

    fix bugs when there is no scan level 2

--- server.js
+++ server.js
@@ -308,6 +308,22 @@ app.get('/getRT', function (req, res) {
         res.end();
     })
 });
+
+app.get('/findNextLevelOneScan', function (req, res) {
+    console.log("Hello, findNextLevelOneScan!");
+    var projectDir = req.query.projectDir;
+    var scan = req.query.scanID;
+    getNextLevelOneScan(projectDir, scan, function (err, row) {
+        if (row !== undefined) {
+            res.write(row.scan.toString());
+            res.end();
+        }else {
+            res.write("0");
+            res.end();
+        }
+
+    })
+});
 // app.use( function(req, res){
 //     console.log('404 handler..');
 //     res.sendFile( __dirname + "/public/" + "404.html" );
@@ -405,7 +421,28 @@ function getRT(dir, scanNum, callback) {
         return callback(null, row);
     });
     resultDb.close();
-};
+}
+function getNextLevelOneScan(dir, scanNum, callback) {
+    let sql = `SELECT SCAN AS scan
+                FROM SPECTRA
+                WHERE SCAN > ? AND SCANLEVEL =2
+                LIMIT 1;`;
+    let dbDir = dir.substr(0, dir.lastIndexOf(".")) + ".db";
+    let resultDb = new sqlite3.Database(dbDir, (err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        // console.log('Connected to the result database.');
+    });
+    resultDb.get(sql,[scanNum], (err, row) => {
+        if (err) {
+            console.error(err.message);
+        }
+        //console.log(row);
+        return callback(null, row);
+    });
+    resultDb.close();
+}
 function getScanLevelTwoList(dir, scanID, callback) {
     let sql = `SELECT LevelTwoScanID AS scanID,
                     PREC_MZ AS prec_mz,
