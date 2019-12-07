72353441b6107ca38020d268668009a104a79f02
jiangtia <jiangtia@iu.edu>
2019-12-06 20:59:00 -0500

    update drawSpectrum library

--- public/js/spectrum_graph/invokespectrum.js
+++ public/js/spectrum_graph/invokespectrum.js
@@ -1,5 +1,5 @@
 /*	Spectrum start point */
-addSpectrum1 = function(peakList,envelopeList,monoMZ){
+addSpectrum = function(id,peakList,envelopeList,monoMZ){
 	let specParameters = new SpectrumParameters();
 	peakList.sort(function(x,y){
 		return d3.ascending(x.mz, y.mz);
@@ -41,54 +41,8 @@ addSpectrum1 = function(peakList,envelopeList,monoMZ){
 	peakData.peak_list = peakList ;
 	
 	peakData.envelope_list = envelopeList ;
-	let spectrumgraph = new SpectrumGraph("#spectrum1",specParameters,peakData);
+	id = "#"+id;
+	let spectrumgraph = new SpectrumGraph(id,specParameters,peakData);
 	 return spectrumgraph;
-}
-
-/*	Spectrum start point */
-addSpectrum2 = function(peakList,envelopeList,monoMZ){
-	let specParameters = new SpectrumParameters();
-	peakList.sort(function(x,y){
-		return d3.ascending(x.mz, y.mz);
-	})
-	let listSize = peakList.length;
-	let minMzData = peakList[0].mz;
-	let maxMzData = peakList[listSize-1].mz;
-
-	peakList.sort(function(x,y){
-		return d3.ascending(x.intensity, y.intensity);
-	})
-	let maxIntensity = peakList[listSize-1].intensity;
-	let minIntensity = peakList[0].intensity;
-	let currminMz = minMzData ;
-	let currmaxMz = maxMzData ;
-	let currentMaxIntensity = maxIntensity ;
-	if(monoMZ != null)
-	{
-		currentMaxIntensity = 0 ;
-		monoMZ = parseInt(monoMZ);
-		currminMz = monoMZ - specParameters.onClickMassAdjacentRange ;
-		currmaxMz = monoMZ + specParameters.onClickMassAdjacentRange ;
-		for(let i = 0; i<listSize ; i++)
-		{
-			if(peakList[i].mz > currminMz && peakList[i].mz < currmaxMz)
-			{
-				if(peakList[i].intensity > currentMaxIntensity)
-				{
-					currentMaxIntensity = peakList[i].intensity;
-				}
-			}
-		}
-	}
-	specParameters.initScale(currminMz,currmaxMz,maxIntensity,minIntensity,minMzData,maxMzData,currentMaxIntensity);
-	let peakData = {};
-	peakList.sort(function(x,y){
-		return d3.descending(x.intensity, y.intensity);
-	})
-	peakData.peak_list = peakList ;
-
-	peakData.envelope_list = envelopeList ;
-	let spectrumgraph = new SpectrumGraph("#spectrum2",specParameters,peakData);
-	return spectrumgraph;
 }
 
