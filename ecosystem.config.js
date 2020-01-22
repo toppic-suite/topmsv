431348d6ce64880d2b76e42088cfd329775835aa
jiangtia <jiangtia@iu.edu>
2020-01-22 13:10:13 -0500

    hide envFile name if no such file and modify README

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,16 @@
 module.exports = {
-    apps : [
-        {
-            name: "top_view",
-            script: "./server.js",
-            watch: true,
-            env: {
-                "NODE_ENV": "development"
-            },
-            env_production: {
-                "NODE_ENV": "production"
-            }
+    apps : [{
+        name: 'top_view',
+        script: './server.js',
+        // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+        instances: 1,
+        autorestart: true,
+        watch: false,
+        env: {
+            NODE_ENV: 'development'
+        },
+        env_production: {
+            NODE_ENV: 'production'
         }
-    ]
-}
+    }]
+};
