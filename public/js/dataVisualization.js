f84d819f18b2456c23964589efb05eee6043f0c7
jiangtia <jiangtia@iu.edu>
2020-02-13 13:31:35 -0500

    validate mslign file upload

--- public/js/dataVisualization.js
+++ public/js/dataVisualization.js
@@ -732,6 +732,16 @@ var xhr = new XMLHttpRequest();
 upload.addEventListener('click', uploadFile, false);
 
 function uploadFile() {
+    if(ms1file.files[0] === undefined || ms2file.files[0] === undefined) {
+        alert("Please choose msalign files for both ms1 and ms2!");
+        return;
+    } else if (!ms1file.files[0].name.match(/.(msalign)$/i)) {
+        alert("Please upload .msalign file for ms1");
+        return;
+    } else if (!ms2file.files[0].name.match(/.(msalign)$/i)) {
+        alert("Please upload .msalign file for ms2");
+        return;
+    }
     var formData = new FormData();
     formData.append('ms1file', ms1file.files[0]);
     formData.append('ms2file', ms2file.files[0]);
