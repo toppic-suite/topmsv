ad25d4ed0b9c4ce01642c7c6033e57eb8ad1bebc
inkwchoi <inkwchoi@iu.edu>
2020-05-26 11:11:07 -0400

    show data by rt range & sync mz range with ms1 graph

--- server.js
+++ server.js
@@ -397,7 +397,8 @@ app.use('/', require("./router/data3D"));
 app.use('/', require("./router/upload3D"));
 app.use('/', require("./router/projects3D"));
 app.use('/', require("./router/load3dData"));
-app.use('/', require("./router/load3dDataByScan"));
+//app.use('/', require("./router/load3dDataByScan"));
+app.use('/', require("./router/load3dDataByRtRange"));
 app.use('/', require("./router/loadDataRange"));
 
 
