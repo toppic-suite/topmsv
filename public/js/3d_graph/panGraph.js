7b43554a919e7d6e32fe71da0cd31528002945e1
inkwchoi <inkwchoi@iu.edu>
2020-06-02 13:39:51 -0400

    panning fixed & tick label removed --incorrect unit

--- public/js/3d_graph/panGraph.js
+++ public/js/3d_graph/panGraph.js
@@ -6,14 +6,14 @@ MsGraph.prototype.panGraph = function(graph){
     this.mdelta = new THREE.Vector3();
 
     graph.renderer.domElement.addEventListener('mousedown', this.onMouseDown.bind(this), false);
-    graph.renderer.domElement.addEventListener('mousemove', this.onMousemove.bind(this), false);
+    //graph.renderer.domElement.addEventListener('mousemove', this.onMousemove.bind(this), false);
     graph.renderer.domElement.addEventListener('mouseup', this.onMouseUp.bind(this), false);
 }
 MsGraph.prototype.panView = function(x,z){
     let viewRange = this.viewRange;
     let mzmin = viewRange.mzmin + (x * viewRange.mzrange);
     let rtmin = viewRange.rtmin + (z * viewRange.rtrange);
-    this.setViewingAreaNoDataLoad(mzmin, viewRange.mzrange, rtmin, viewRange.rtrange);
+    this.setViewingArea(mzmin, viewRange.mzrange, rtmin, viewRange.rtrange);
 }
 MsGraph.prototype.onMouseDown = function(e){
     if (e.button === 0) {
@@ -25,45 +25,17 @@ MsGraph.prototype.onMouseDown = function(e){
         this.mstart.copy(mousePoint);
       }
 }
-MsGraph.prototype.onMousemove = function(e){
-    let mousePoint = this.getMousePosition(e);
-    if (mousePoint === null) {
-      return;
-    }
-    
-    if (this.mstart) {
-      this.mend.copy(mousePoint);
-      this.mdelta.subVectors(this.mend, this.mstart);
-      
-      this.panView(-this.mdelta.x, -this.mdelta.z);
-      this.mstart.copy(this.mend);
-    }
-/*
-    // find point closest to mouse
-    let vr = this.viewRange;
-    let mouseMz = mousePoint.x * vr.mzrange + vr.mzmin;
-    let mouseRt = mousePoint.z * vr.rtrange + vr.rtmin;
-
-    let closest, closestDist = Number.MAX_VALUE;
-    this.linesArray.forEach(function(line) {
-      let mzD = Math.abs(line.mz - mouseMz);
-      let rtD = Math.abs(line.rt - mouseRt);
-
-      // only consider points within 0.5 m/z and RT units, then go by closest distance to mouse
-      if (mzD > 0.5 || rtD > 0.5) {
-        return;
-      }
-
-      let dist = Math.sqrt(mzD * mzD + rtD * rtD);
-      if (dist < closestDist) {
-        closest = line;
-        closestDist = dist;
-      }
-    });*/
-}
 MsGraph.prototype.onMouseUp = function(e) {
-    if (e.button === 0) {
-      this.mstart = null;
-    }
-    console.log("panning over")
+  let mousePoint = this.getMousePosition(e);
+  if (mousePoint === null) {
+    return;
   }
+  if (this.mstart) {
+    this.mend.copy(mousePoint);
+    this.mdelta.subVectors(this.mend, this.mstart);
+    
+    this.panView(-this.mdelta.x, -this.mdelta.z);
+    this.mstart.copy(this.mend);
+  }
+  this.mstart = null;
+}
