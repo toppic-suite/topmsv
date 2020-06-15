5cc00a8613a02e0e3d2dd4c23745c0f14a3de047
inkwchoi <inkwchoi@iu.edu>
2020-06-15 15:34:48 -0400

    multi layer tables connected with front end

--- library/load3dDataByParaRange.js
+++ library/load3dDataByParaRange.js
@@ -7,9 +7,35 @@ const sqlite3 = require('sqlite3').verbose();
  * @async
  */
 function load3dDataByParaRange(dir, minrt, maxrt, minmz, maxmz, callback) {
+    //based on mz rt range, select which table to use
+    //assume that in mz range of 10, there are 10 peaks, 
+    //assume that in rt range of 0.25, there are 5 scans
+
+    //const peakCount = [3000, 18750, 75000, 300000, 768000, 1875000];
+    const peakCount = [30, 175, 200, 250, 300, 400];//max peaks in each table from mzMLReader3D.hpp
+    
+    let peakPerMz = 1;
+    let scanPerRt = 20;
+    let expectedPeaks = Math.ceil((maxmz - minmz) * peakPerMz);
+    let expectedScans = Math.ceil((maxrt - minrt) * scanPerRt);
+    let totalExpectedPeaks = expectedPeaks * expectedScans; 
+    let minDiff = Infinity;
+    let tableNum = 0;
+
+    for (let i = 0; i < peakCount.length; i++){
+        //find which level has the closet number of peaks with totalExpectedPeaks
+        let diff = Math.abs(peakCount[i] - totalExpectedPeaks);
+        if (diff < minDiff ){
+            tableNum = i;
+            minDiff = diff;
+        } 
+    }
+    console.log("total expected peakcount is :" , totalExpectedPeaks);
+    console.log("the selected table is PEAKS" , tableNum);
+
     let sql = `SELECT *
-                FROM PEAKS
-                WHERE RETENTIONTIME <= ? 
+                FROM PEAKS` + tableNum.toString() + 
+                ` WHERE RETENTIONTIME <= ? 
                 AND RETENTIONTIME >= ?
                 AND MZ <= ?
                 AND MZ >= ?;`;
