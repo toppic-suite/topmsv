87eac632650868ac0441dd674b0da003423885ce
inkwon <inkwchoi@iu.edu>
2020-08-06 12:59:43 -0400

    can upload mzrt file

--- public/js/result.js
+++ public/js/result.js
@@ -986,6 +986,20 @@ $("#deleteMsalign").click(function () {
         });
     }
 });
+$("#deleteMzrt").click(function () {
+    var result = confirm("Are you sure that you want to delete mzrt data?");
+    if (result) {
+        $.ajax({
+            url:"deleteMzrt?projectDir=" + document.getElementById("projectDir").value+ "&projectCode=" + document.getElementById('projectCode').value,
+            type: "get",
+            // dataType: 'json',
+            success: function (res) {
+                alert('Your previous mzrt data has been removed.');
+                location.reload();
+            }
+        });
+    }
+});
 $("#deleteSeq").click(function () {
     var result = confirm("Are you sure that you want to delete sequence data?");
     if (result) {
@@ -1043,7 +1057,46 @@ $("#seqUpload").click(function () {
         }
     }
 });
+$("#mzrtUpload").click(function () {
+    var mzrtFile = document.querySelector('#mzrtFile');
+    var mzrtProgress = document.querySelector('#mzrtProgressbar');
+    var xhr = new XMLHttpRequest();
+    if(mzrtFile.files[0] === undefined) {
+        alert("Please choose a mzrt file first!");
+        return;
+    } else if (!mzrtFile.files[0].name.match(/.(csv)$/i)) {
+        alert('Please upload a csv file!');
+        return;
+    }
+    var formData = new FormData();
+    formData.append('mzrtFile', mzrtFile.files[0]);
+    formData.append('projectDir', document.getElementById('projectDir').value);
+    formData.append('projectCode',document.getElementById("projectCode").value);
+    formData.append('projectName', document.getElementById("projectName").value);
+    formData.append('email', document.getElementById("email").value);
+    xhr.upload.onprogress = mzrtSetProgress;
+    xhr.onload = mzrtUploadSuccess;
+    xhr.open('post', '/mzrt', true);
+    xhr.send(formData);
 
+    function mzrtUploadSuccess(event) {
+        if (xhr.readyState === 4) {
+            alert("Upload successfully!");
+            window.location.replace("/projects");
+        }
+    }
+
+    function mzrtSetProgress(event) {
+        if (event.lengthComputable) {
+            var complete = Number.parseInt(event.loaded / event.total * 100);
+            mzrtProgress.style.width = complete + '%';
+            mzrtProgress.innerHTML = complete + '%';
+            if (complete == 100) {
+                mzrtProgress.innerHTML = 'Done!';
+            }
+        }
+    }
+});
 var ms1file = document.querySelector('#MS1_msalign');
 var ms2file = document.querySelector('#MS2_msalign');
 var upload = document.querySelector('#modalUpload');
