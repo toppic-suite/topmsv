fca209b8e5a3a6a8c4a1e7a4a102ff080ed4ca29
inkwon <inkwchoi@iu.edu>
2020-08-12 12:30:59 -0400

    tooltip position fixed

--- public/js/3d_graph/hoverFeature.js
+++ public/js/3d_graph/hoverFeature.js
@@ -17,9 +17,6 @@ MsGraph.prototype.findObjectHover = function(event){
     raycaster.setFromCamera(coord, this.resizedCamera);
 
     var intersects = raycaster.intersectObjects( this.scene.getObjectByName("featureGroup").children );
-    //for ( var i = 0; i < intersects.length; i++ ) {
-        //console.log( intersects[ i ] ); 
-    //}
     if (intersects.length > 0){
         return intersects[0].object;
     }
@@ -34,9 +31,10 @@ MsGraph.prototype.onMouseOver = function(event){
         document.getElementById("tooltip").style.display = "inline-block";
         document.getElementById("tooltip").style.top = (event.clientY - 10) + 'px';
         document.getElementById("tooltip").style.left = (event.clientX + 20) + 'px';
-        document.getElementById("tooltiptext").innerHTML = "mass: " + obj.mass + "<br/>" + "mono_mz: " + obj.mono_mz + 
-        "<br/>" + "charge: " + obj.charge + "<br/>" + "intensity: " + obj.intensity;
-        
+        //document.getElementById("tooltiptext").innerHTML = "mass: " + obj.mass + "<br/>" + "mono_mz: " + obj.mono_mz + 
+        //"<br/>" + "charge: " + obj.charge + "<br/>" + "intensity: " + obj.intensity;
+        document.getElementById("tooltiptext").innerHTML = "mz_low: " + obj.mz_low + "<br/>" + "mz_high: " + obj.mz_high + 
+        "<br/>" + "rt_low: " + obj.rt_low + "<br/>" + "rt_high: " + obj.rt_high;
     }
     else{
         document.getElementById("tooltip").style.display = "none";
