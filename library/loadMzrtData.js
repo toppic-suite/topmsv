19abe463608f90b392d79d1a26dc586700769a61
inkwon <inkwchoi@iu.edu>
2020-08-12 10:41:52 -0400

    feature scaling fixed

--- library/loadMzrtData.js
+++ library/loadMzrtData.js
@@ -7,18 +7,9 @@ const sqlite3 = require('sqlite3').verbose();
  * @async
  */
 function loadMzrtData(dir, minrt, maxrt, minmz, maxmz, callback) {
-    let sql = `SELECT * FROM feature
-                WHERE rt_high <= ? 
-                AND rt_low >= ?
-                AND mz_high <= ?
-                AND mz_low >= ?
-                ORDER BY INTENSITY DESC
-                LIMIT 500;`;     
-    /*let sql = `SELECT * FROM feature
-                WHERE (mz_low <= ? AND rt_low <= ?)
-                OR (mz_low <= ? AND rt_high >= ?)
-                OR (mz_high >= ? AND rt_low <= ?)
-                OR (mz_high >= ? AND rt_high >= ?);`;    */   
+   let sql = `SELECT * FROM feature
+              WHERE NOT (rt_low >= ? OR rt_high <= ? OR mz_low >= ? OR mz_high <= ?)
+              LIMIT 500;`;          
     let dbDir = dir;
     let resultDb = new sqlite3.Database(dbDir, (err) => {
         if (err) {
@@ -28,7 +19,6 @@ function loadMzrtData(dir, minrt, maxrt, minmz, maxmz, callback) {
     });
 
     resultDb.all(sql, [maxrt, minrt, maxmz, minmz], (err, row) => {
-    //resultDb.all(sql, [maxmz, maxrt, maxmz, minrt, minmz, maxrt, minmz, minrt], (err, row) => {
         if (err) {
             console.error(err.message);
         }
