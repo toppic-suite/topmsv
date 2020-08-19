28881d1e21fe5ee1d74562cef0a944daa4c74d82
inkwchoi <inkwchoi@iu.edu>
2020-08-19 14:52:58 -0400

    hover added & running without error

---
+++ public/js/3d_graph/graph_interaction/hover_feature.js
@@ -0,0 +1,23 @@
+//on hover, highlught and display feature information (scan ID, intensity, rt, mz)
+class HoverFeature{
+    constructor(){};
+    onMouseOver = function(event){
+        let objGroup = Graph.scene.getObjectByName("featureGroup");
+        let obj = GraphUtil.findObjectHover(event, objGroup);
+        if (obj != null){
+            
+            
+            document.getElementById("tooltip").style.display = "inline-block";
+            document.getElementById("tooltip").style.top = (event.clientY - 10) + 'px';
+            document.getElementById("tooltip").style.left = (event.clientX + 20) + 'px';
+            document.getElementById("tooltiptext").innerHTML = "mass: " + obj.mass + "<br/>" + "mono_mz: " + obj.mono_mz + 
+            "<br/>" + "charge: " + obj.charge + "<br/>" + "intensity: " + GraphUtil.formatScientificNotation(obj.intensity);
+        }
+        else{
+            document.getElementById("tooltip").style.display = "none";
+        }
+    }
+    init(){
+        Graph.renderer.domElement.addEventListener('mousemove', this.onMouseOver.bind(this), false);
+    }
+}
