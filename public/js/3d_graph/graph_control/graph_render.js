823afaf9b5100864faf21f0fc61685b302ee1d2a
inkwchoi <inkwchoi@iu.edu>
2020-09-02 13:14:56 -0400

    fixed #184

--- public/js/3d_graph/graph_control/graph_render.js
+++ public/js/3d_graph/graph_control/graph_render.js
@@ -1,15 +1,19 @@
 class GraphRender{
     constructor(){}
-    
-    static renderImmediate = () => {
+    static checkAndRender = () => {
         if (Graph.camera.position.y > 25.495){
+            Graph.isPerpendicular = true;
             //if camera is perpendicular to view, switch to cylinder group
             GraphData.plotPointAsCircle();
         }else{
+            Graph.isPerpendicular = false;
             let dataGroup = Graph.scene.getObjectByName("dataGroup");
             let prevGroup = dataGroup.getObjectByName("cylinderGroup");
             dataGroup.remove(prevGroup);
         }
+        this.renderImmediate();
+    }
+    static renderImmediate = () => {        
         Graph.renderer.render( Graph.scene, Graph.camera );
         Graph.imageAddress = Graph.renderer.domElement.toDataURL();
     }
