02a6f5c4e7096cb22503a91bd020a3ef8209ef1a
inkwchoi <inkwchoi@iu.edu>
2020-09-24 12:06:15 -0400

    removed empty space & buttons for size control

--- public/js/3d_graph/graph_control/graph_control.js
+++ public/js/3d_graph/graph_control/graph_control.js
@@ -58,8 +58,6 @@ class GraphControl{
         dataGroup.position.set(-r.mzmin*mz_squish, 0, Graph.gridRange - r.rtmin*rt_squish);
         markerGroup.position.set(0, 0, Graph.gridRange - r.rtmin*rt_squish);
         
-        //console.log(-r.mzmin*mz_squish, 0, Graph.gridRange - r.rtmin*rt_squish)
-
         // update tick marks
         GraphUtil.emptyGroup(tickLabelGroup);
 
@@ -205,11 +203,14 @@ class GraphControl{
             rtmin: newrtmin, rtmax: newrtmax, rtrange: newrtrange,
         }
     }
-    static resizeCameraWhenExpanded = () => {
-        Graph.renderer.setSize(window.innerWidth * 0.8, window.innerHeight, true);
-        let aspectRatio = Graph.renderer.getSize().width / Graph.renderer.getSize().height;
+    static resizeCameraUserControl = (windowAdjust) => {
+        //Graph.renderer.setSize(Graph.graphEl.clientWidth * windowAdjust, Graph.graphEl.clientHeight * windowAdjust, true);
 
-        let vs = Graph.viewSize;
+        let size = new THREE.Vector3();
+        Graph.renderer.getSize(size);
+        let aspectRatio = size.x / size.y;
+
+        let vs = Graph.viewSize ;
         if (aspectRatio > 1) 
         {
             // width greater than height; scale height to view size to fit content
@@ -236,7 +237,9 @@ class GraphControl{
     }
     static resizeCamera = () => {
         Graph.renderer.setSize(Graph.graphEl.clientWidth, Graph.graphEl.clientHeight, true);
-        let aspectRatio = Graph.renderer.getSize().width / Graph.renderer.getSize().height;
+        let size = new THREE.Vector3();
+        Graph.renderer.getSize(size);
+        let aspectRatio = size.x / size.y;
 
         let vs = Graph.viewSize;
         if (aspectRatio > 1) 
