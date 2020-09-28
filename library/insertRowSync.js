06e6b3287c4e19c27a62e2b3fb37f85bcc0d3f5f
inkwchoi <inkwchoi@iu.edu>
2020-09-28 10:22:13 -0400

    fixed #262 & removed unnecessary std::couts

--- library/insertRowSync.js
+++ library/insertRowSync.js
@@ -18,11 +18,6 @@ const BetterDB = require("better-sqlite3");
 function insertRowSync(ProjectCode, ProjectName, FileName, Description, ProjectDir, ProjectStatus, Email, EnvStatus, FeatureStatus, SeqStatus, ms1EnvFile,uid,publicStatus) {
     let resultDb = new BetterDB('./db/projectDB.db');
     let stmt = resultDb.prepare('INSERT INTO Projects(ProjectCode, ProjectName, FileName, Description, ProjectDir, ProjectStatus, Email, EnvelopeStatus, FeatureStatus, SequenceStatus, MS1_envelope_file, uid, public) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)');
-    
-    
-    console.log(ProjectCode, ProjectName, FileName, Description, ProjectDir, ProjectStatus, Email, EnvStatus, FeatureStatus,SeqStatus, ms1EnvFile,uid, publicStatus)
-    
-    
     let info = stmt.run(ProjectCode, ProjectName, FileName, Description, ProjectDir, ProjectStatus, Email, EnvStatus, FeatureStatus,SeqStatus, ms1EnvFile,uid, publicStatus);
     console.log("insertRowSync info", info);
     resultDb.close();
