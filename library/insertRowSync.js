c9bb1c59597ed5a3921ede0f48f8491030153416
inkwchoi <inkwchoi@iu.edu>
2020-09-28 09:45:22 -0400

    fixed problem with FeatureStatus

--- library/insertRowSync.js
+++ library/insertRowSync.js
@@ -9,6 +9,7 @@ const BetterDB = require("better-sqlite3");
  * @param {number} ProjectStatus
  * @param {string} Email
  * @param {number} EnvStatus
+ * @param {number} FeatureStatus
  * @param {number} SeqStatus
  * @param {string} ms1EnvFile
  * @param {string} uid
@@ -17,6 +18,11 @@ const BetterDB = require("better-sqlite3");
 function insertRowSync(ProjectCode, ProjectName, FileName, Description, ProjectDir, ProjectStatus, Email, EnvStatus, FeatureStatus, SeqStatus, ms1EnvFile,uid,publicStatus) {
     let resultDb = new BetterDB('./db/projectDB.db');
     let stmt = resultDb.prepare('INSERT INTO Projects(ProjectCode, ProjectName, FileName, Description, ProjectDir, ProjectStatus, Email, EnvelopeStatus, FeatureStatus, SequenceStatus, MS1_envelope_file, uid, public) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)');
+    
+    
+    console.log(ProjectCode, ProjectName, FileName, Description, ProjectDir, ProjectStatus, Email, EnvStatus, FeatureStatus,SeqStatus, ms1EnvFile,uid, publicStatus)
+    
+    
     let info = stmt.run(ProjectCode, ProjectName, FileName, Description, ProjectDir, ProjectStatus, Email, EnvStatus, FeatureStatus,SeqStatus, ms1EnvFile,uid, publicStatus);
     console.log("insertRowSync info", info);
     resultDb.close();
