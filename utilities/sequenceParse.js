a87d1463043ba09423e8a12dbad007d6b47a74f0
jiangtia <jiangtia@iu.edu>
2020-10-05 23:32:07 -0400

    replace var with let or const

--- utilities/sequenceParse.js
+++ utilities/sequenceParse.js
@@ -1,7 +1,7 @@
-var Papa = require('papaparse');
+const Papa = require('papaparse');
 const Database = require('better-sqlite3');
-var fs = require('fs');
-var myArgs = process.argv.slice(2);
+const fs = require('fs');
+const myArgs = process.argv.slice(2);
 const parameter = '********************** Parameters **********************';
 const betterDB = new Database(myArgs[0]);
 const stmtCreateSequenceTable = betterDB.prepare('CREATE TABLE IF NOT EXISTS sequence (\n' +
@@ -15,20 +15,20 @@ const stmtCreateSequenceTable = betterDB.prepare('CREATE TABLE IF NOT EXISTS seq
 stmtCreateSequenceTable.run();
 const insertMany = betterDB.transaction(importData);
 
-var file = fs.readFileSync(myArgs[1], "utf-8");
+let file = fs.readFileSync(myArgs[1], "utf-8");
 file = findCSV(file);
 
 insertMany(betterDB, file);
 
-var stmtSeqIndex = betterDB.prepare("CREATE INDEX IF NOT EXISTS `sequence_index` ON `sequence` ( `scan_id` )");
+const stmtSeqIndex = betterDB.prepare("CREATE INDEX IF NOT EXISTS `sequence_index` ON `sequence` ( `scan_id` )");
 stmtSeqIndex.run();
 betterDB.close();
 
 function importData(db, data) {
-    var stmtFindScanID = db.prepare('SELECT ID AS id FROM SPECTRA WHERE SCAN = ?');
-    var stmtInsert = db.prepare('INSERT INTO sequence(id,scan_id,protein_accession,proteoform) VALUES(?,?,?,?)');
-    var stmtMaxSeqID = db.prepare('SELECT MAX(id) AS maxID FROM sequence');
-    var id = stmtMaxSeqID.get().maxID + 1;
+    const stmtFindScanID = db.prepare('SELECT ID AS id FROM SPECTRA WHERE SCAN = ?');
+    const stmtInsert = db.prepare('INSERT INTO sequence(id,scan_id,protein_accession,proteoform) VALUES(?,?,?,?)');
+    const stmtMaxSeqID = db.prepare('SELECT MAX(id) AS maxID FROM sequence');
+    let id = stmtMaxSeqID.get().maxID + 1;
     Papa.parse(data, {
         complete: function(results) {
             // console.log("Finished:", results);
@@ -37,12 +37,12 @@ function importData(db, data) {
             parseResult.forEach(row => {
                 // console.log(row);
                 // console.log('Scans:', row[4]);
-                var scan = row[4];
+                let scan = row[4];
                 // console.log('Proteoform:', row[17]);
-                var protein_accession = row[13];
-                var proteoform = row[17];
+                let protein_accession = row[13];
+                let proteoform = row[17];
                 if(scan !== 'Scan(s)'){
-                    var scan_id = stmtFindScanID.get(scan).id;
+                    let scan_id = stmtFindScanID.get(scan).id;
                     stmtInsert.run(id, scan_id,protein_accession, proteoform);
                     id++;
                 }
@@ -53,7 +53,7 @@ function importData(db, data) {
 
 function findCSV(data) {
     data = data.substring(parameter.length+1);
-    var indexBegin = data.indexOf(parameter) + parameter.length + 1;
+    let indexBegin = data.indexOf(parameter) + parameter.length + 1;
     data = data.trim();
     return data.slice(indexBegin);
 }
