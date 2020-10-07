e467c809b583933263e238867ec83cd3afd9ab18
Tianze Jiang <jiangtia@iu.edu>
2020-10-07 13:41:06 -0400

    update email_sender

--- library/email_sender.js
+++ library/email_sender.js
@@ -4,25 +4,7 @@ const nodemailer = require('nodemailer');
  * Class representing a email sender
  */
 class EmailSender {
-    transport = nodemailer.createTransport({
-        host: "smtp-mail.outlook.com", // hostname
-        secureConnection: false, // TLS requires secureConnection to be false
-        port: 587, // port for secure SMTP
-        tls: {
-            ciphers:'SSLv3'
-        },
-        auth: {
-            user: 'datalink_sender@outlook.com',
-            pass: 'iupuiSOICWK316'
-        }
-    });
     
-    message = {
-        from: 'datalink_sender@outlook.com', // Sender address
-        to: 'default@gmail.com',         // List of recipients
-        subject: 'Default Subject', // Subject line
-        text: 'Default text' // Plain text body
-    };
 
     /**
      * Create an email sender
@@ -31,6 +13,25 @@ class EmailSender {
      * @param {string} emailAddress 
      */
     constructor(subject, text, emailAddress) {
+        this.transport = nodemailer.createTransport({
+            host: "smtp-mail.outlook.com", // hostname
+            secureConnection: false, // TLS requires secureConnection to be false
+            port: 587, // port for secure SMTP
+            tls: {
+                ciphers:'SSLv3'
+            },
+            auth: {
+                user: 'datalink_sender@outlook.com',
+                pass: 'iupuiSOICWK316'
+            }
+        });
+        
+        this.message = {
+            from: 'datalink_sender@outlook.com', // Sender address
+            to: 'default@gmail.com',         // List of recipients
+            subject: 'Default Subject', // Subject line
+            text: 'Default text' // Plain text body
+        };
         this.message.subject = subject;
         this.message.text = text;
         this.message.to = emailAddress;
