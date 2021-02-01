b5cf99ed0bba1e8543b7d565eec254ceec1fe9f6
inkwchoi <inkwchoi@iu.edu>
2021-02-01 11:03:44 -0500

    fix #369 - add FDR information

--- library/getProteoform.js
+++ library/getProteoform.js
@@ -8,13 +8,19 @@ const BetterDB = require("better-sqlite3");
 function getProteoform(dir, scanNum) {
     let dbDir = dir.substr(0, dir.lastIndexOf(".")) + ".db";
     let resultDb = new BetterDB(dbDir);
-    let stmt = resultDb.prepare(`SELECT sequence.proteoform AS proteoform
+    /*let stmt = resultDb.prepare(`SELECT sequence.proteoform AS proteoform
+                FROM SPECTRA INNER JOIN sequence ON SPECTRA.ID = sequence.scan_id
+                WHERE SPECTRA.SCAN = ?`);*/
+    let stmt = resultDb.prepare(`SELECT sequence.proteoform AS proteoform, sequence.spec_fdr AS spec_fdr, sequence.prot_fdr AS prot_fdr
                 FROM SPECTRA INNER JOIN sequence ON SPECTRA.ID = sequence.scan_id
                 WHERE SPECTRA.SCAN = ?`);
     if(stmt.get(scanNum)) {
         let proteoform = stmt.get(scanNum).proteoform;
+        let specFDR = stmt.get(scanNum).spec_fdr;
+        let protFDR = stmt.get(scanNum).prot_fdr;
         resultDb.close();
-        return proteoform;
+        return {"seq":proteoform,"specFDR":specFDR, "protFDR":protFDR};
+        //return proteoform;
     } else {
         resultDb.close();
         return 0;
--- utilities/sequenceParse.js
+++ utilities/sequenceParse.js
@@ -3,17 +3,24 @@ const Database = require('better-sqlite3');
 const fs = require('fs');
 const myArgs = process.argv.slice(2);
 const parameter = '********************** Parameters **********************';
+const specFDR = 'Spectrum-level cutoff value';
+const protFDR = 'Proteoform-level cutoff value';
 const betterDB = new Database(myArgs[0]);
 const stmtCreateSequenceTable = betterDB.prepare('CREATE TABLE IF NOT EXISTS sequence (\n' +
     '    id INTEGER PRIMARY KEY,\n' +
     '    scan_id INTEGER NOT NULL,\n' +
     '    protein_accession TEXT NULL,\n' +
     '    proteoform TEXT NULL,\n' +
+    '    spec_fdr FLOAT NULL,\n' +
+    '    prot_fdr FLOAT NULL,\n' +
     '    FOREIGN KEY (scan_id)\n' +
     '       REFERENCES SPECTRA (ID)\n' +
     ');');
 stmtCreateSequenceTable.run();
 const insertMany = betterDB.transaction(importData);
+
+let specFDRValue = -1;
+let protFDRValue = -1; 
 
 let file = fs.readFileSync(myArgs[1], "utf-8");
 file = findCSV(file);
@@ -26,7 +33,7 @@ betterDB.close();
 
 function importData(db, data) {
     const stmtFindScanID = db.prepare('SELECT ID AS id FROM SPECTRA WHERE SCAN = ?');
-    const stmtInsert = db.prepare('INSERT INTO sequence(id,scan_id,protein_accession,proteoform) VALUES(?,?,?,?)');
+    const stmtInsert = db.prepare('INSERT INTO sequence(id,scan_id,protein_accession,proteoform,spec_fdr, prot_fdr) VALUES(?,?,?,?,?,?)');
     const stmtMaxSeqID = db.prepare('SELECT MAX(id) AS maxID FROM sequence');
     let id = stmtMaxSeqID.get().maxID + 1;
     Papa.parse(data, {
@@ -34,6 +41,9 @@ function importData(db, data) {
             // console.log("Finished:", results);
             let parseResult = results.data.slice(1);
             // console.log("parseResult", parseResult);
+            fs.writeFile('prsm_data_1.txt', parseResult, function (err) {
+                if (err) return console.log(err);
+              });
             parseResult.forEach(row => {
                 // console.log(row);
                 // console.log('Scans:', row[4]);
@@ -41,9 +51,10 @@ function importData(db, data) {
                 // console.log('Proteoform:', row[17]);
                 let protein_accession = row[13];
                 let proteoform = row[17];
+                
                 if(scan !== 'Scan(s)'){
                     let scan_id = stmtFindScanID.get(scan).id;
-                    stmtInsert.run(id, scan_id,protein_accession, proteoform);
+                    stmtInsert.run(id, scan_id,protein_accession, proteoform, specFDRValue, protFDRValue);
                     id++;
                 }
             })
@@ -55,5 +66,18 @@ function findCSV(data) {
     data = data.substring(parameter.length+1);
     let indexBegin = data.indexOf(parameter) + parameter.length + 1;
     data = data.trim();
+    
+    let paraData = data.substring(0, indexBegin);
+    let fdr = paraData.indexOf('FDR');
+
+    if (fdr > -1){
+        let protFDRIndex = paraData.indexOf(protFDR);
+        let specFDRIndex = paraData.indexOf(specFDR);
+        let specFDRVal = paraData.slice((specFDRIndex + specFDR.length + 1), paraData.indexOf('\n',specFDRIndex));
+        let protFDRVal = paraData.slice((protFDRIndex + protFDR.length + 1), paraData.indexOf('\n',protFDRIndex));
+
+        specFDRValue = parseFloat(specFDRVal);
+        protFDRValue = parseFloat(protFDRVal);
+    }
     return data.slice(indexBegin);
 }
