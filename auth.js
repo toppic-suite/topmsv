d95af53c847f96c0997f77a52fa76a69948dd928
inkwon <inkwchoi@iu.edu>
2021-02-23 09:00:48 -0500

    remove mzidGenerator folder

--- auth.js
+++ auth.js
@@ -10,8 +10,8 @@ module.exports = function (passport) {
     passport.use(new GoogleStrategy({
         clientID: '276824870427-fgoth5ua8olchdtrhdqp6bqokr1plgpv.apps.googleusercontent.com',
         clientSecret: "YlS759x13UDDt1yJTtYy9yrf",
-        callbackURL: 'https://toppic.soic.iupui.edu/auth/google/callback'
-        // callbackURL: 'http://localhost:8443/auth/google/callback'
+        //callbackURL: 'https://toppic.soic.iupui.edu/auth/google/callback'
+        callbackURL: 'http://localhost:8443/auth/google/callback'
     }, (token, refreshToken, profile, done) => {
         return done(null, {
             profile: profile,
