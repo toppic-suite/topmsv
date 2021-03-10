d880e27764aa41dc2eb122eb44580cdc9081304c
inkwon <inkwchoi@iu.edu>
2021-03-10 09:06:19 -0500

    updated to toppic 1.4.9

--- auth.js
+++ auth.js
@@ -8,10 +8,9 @@ module.exports = function (passport) {
         done(null, user);
     });
     passport.use(new GoogleStrategy({
-        clientID: '276824870427-fgoth5ua8olchdtrhdqp6bqokr1plgpv.apps.googleusercontent.com',
-        clientSecret: "YlS759x13UDDt1yJTtYy9yrf",
-        //callbackURL: 'https://toppic.soic.iupui.edu/auth/google/callback'
-        callbackURL: 'http://localhost:8443/auth/google/callback'
+        clientID: "GOOGLE_CLIENT_ID",
+        clientSecret: "GOOGLE_CLIENT_SECRET",
+        callbackURL: 'http://localhost:8443/auth/google/callback'//replace localhost with server address when publsihing the app on the server 
     }, (token, refreshToken, profile, done) => {
         return done(null, {
             profile: profile,
