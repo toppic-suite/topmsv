8035a470855fbc76afad1777709002cac00593e6
inkwon <inkwchoi@iu.edu>
2021-03-19 11:33:11 -0400

    add TopMSV to resources folder manually

---
+++ public/resources/topview/common/prsm/prsm.js
@@ -0,0 +1,53 @@
+class Prsm {
+  constructor(id, proteoform, ms1Spec, ms2Spec, breakPoints, matchedPeaks = -1, matchedIons = -1, eValue = -1, qValue = -1) {
+    this.id_ = id;
+    this.proteoform_ = proteoform;
+    this.ms1Spec_ = ms1Spec;
+    this.ms2Spec_ = ms2Spec;
+    this.breakPoints_ = breakPoints;
+    this.matchedPeaks_ = parseInt(matchedPeaks);
+    this.matchedIons_ = parseInt(matchedIons);
+    //combine matched* into one matchedpeakpair
+    this.eValue_ = parseFloat(eValue);
+    this.qValue_ = parseFloat(qValue);
+  }
+  getId() {
+    return this.id_;
+  }
+  getProteoform() {
+    return this.proteoform_;
+  }
+  getMs1Spectra() {
+    return this.ms1Spec_;
+  }
+  getMs2Spectra() {
+    return this.ms2Spec_;
+  }
+  getMatchedPeakCount() {
+    return this.matchedPeaks_;
+  }
+  getMatchedFragIonCount() {
+    return this.matchedIons_;
+  }
+  getUnexpectedModCount() {
+    let protObj = this.getProteoform();
+    let unexpectedMod = protObj.getUnknownMassShift();
+    //can be retrieved from proteoform
+    return unexpectedMod.length;
+  }
+  getEValue() {
+    return this.eValue_;
+  }
+  getQValue() {
+    return this.qValue_;
+  }
+  getBreakPoints() {
+    return this.breakPoints_;
+  }
+  setBreakPoints(breakPoints) {
+    this.breakPoints_ = breakPoints;
+  }
+  setProteoform(proteoform) {
+    this.proteoform_ = proteoform;
+  }
+}
