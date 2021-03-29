f22f393d59b8353adfe6e079207fcc08b4b16e77
inkwon <inkwchoi@iu.edu>
2021-03-29 09:18:37 -0400

    updated nodemailer-auth.js

---
+++ nodemailer-auth.js
@@ -0,0 +1,24 @@
+const nodemailer = require('nodemailer');
+
+const transport = nodemailer.createTransport({
+    host: HOSTNAME, // hostname
+    secureConnection: false, // TLS requires secureConnection to be false
+    port: 587, // port for secure SMTP
+    tls: {
+        ciphers:'SSLv3'
+    },
+    auth: {
+        user: USERNAME,
+        pass: PW
+    }
+});
+
+const message = {
+    from: YOUR_EMAIL,
+    to: 'default@gmail.com',         // List of recipients
+    subject: 'Default Subject', // Subject line
+    text: 'Default text' // Plain text body
+};
+
+module.exports.transport = transport;
+module.exports.message = message;
