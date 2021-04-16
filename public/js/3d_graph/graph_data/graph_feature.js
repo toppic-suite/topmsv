d585b605db9622ce86fe945be4274d39b38e3f3d
inkwon <inkwchoi@iu.edu>
2021-04-16 12:26:30 -0400

    fix feature annotation pos when panning

--- public/js/3d_graph/graph_data/graph_feature.js
+++ public/js/3d_graph/graph_data/graph_feature.js
@@ -2,9 +2,10 @@
 class GraphFeature{
     constructor(){}
     /******** ADD FEAUTRE ANNOTATION ******/
-    static updateFeature = (data, minmz, maxmz, minrt, maxrt) => {
+    static updateFeature = (minmz, maxmz, minrt, maxrt) => {
         let featureGroup = Graph.scene.getObjectByName("featureGroup");
-        
+        //console.log(minmz, maxmz, minrt, maxrt)
+        let data = Graph.currentFeatureData;
         featureGroup.children.forEach(function(featureRect, index) {
             if (index < data.length) {
                 let feature = data[index];
@@ -63,67 +64,6 @@ class GraphFeature{
             }
         })
     }
-    /*static addFeatureToGraph = (featureData, minmz, maxmz, minrt, maxrt) => {
-        //draw rectangle (4 separate lines) from each feature Data
-        let featureGroup = Graph.scene.getObjectByName("featureGroup");
-        for (let i = 0; i < featureData.length; i++){
-            let data = featureData[i];
-            let linemat = new THREE.LineDashedMaterial( { color: Graph.featureColor, dashSize: 0.01, gapSize: 0.005 } )
-            let points = [];
-    
-            let mz_low = data.mz_low;
-            let mz_high = data.mz_high;
-            let rt_low = data.rt_low;
-            let rt_high = data.rt_high;
-    
-            if (mz_low < minmz){
-                mz_low = minmz;
-            }
-            if (mz_high > maxmz){
-                mz_high = maxmz;
-            }
-            if (rt_low < minrt){
-                rt_low = minrt;
-            }
-            if (rt_high > maxrt){
-                rt_high = maxrt;
-            }
-    
-            rt_high = rt_high * 60;
-            rt_low = rt_low * 60;
-    
-            //adjust point position when it goes outside of the graph range
-            points.push( new THREE.Vector3(mz_low, 0.1, rt_high) );
-            points.push( new THREE.Vector3(mz_high, 0.1, rt_high) );
-            points.push( new THREE.Vector3(mz_high, 0.1, rt_low) );
-            points.push( new THREE.Vector3(mz_low, 0.1, rt_low) );
-            points.push( new THREE.Vector3(mz_low, 0.1, rt_high) );
-    
-            let geometry = new THREE.BufferGeometry().setFromPoints( points );
-    
-            let feature = new THREE.Line( geometry, linemat );
-            feature.computeLineDistances();
-    
-            feature.mz_low = data.mz_low;
-            feature.mz_high = data.mz_high;
-            feature.rt_low = data.rt_low;
-            feature.rt_high = data.rt_high;
-    
-            feature.mass = data.mass;
-            feature.mono_mz = data.mono_mz;
-            feature.charge = data.charge;
-            feature.intensity = data.intensity;
-    
-            feature.name = "featureAnnotation";
-            featureGroup.add(feature);
-        }
-        let mz_squish = Graph.gridRange / Graph.viewRange.mzrange;
-        let rt_squish = - Graph.gridRange / Graph.viewRange.rtrange;
-
-        featureGroup.scale.set(mz_squish, 1, rt_squish);
-        // Reposition the plot so that mzmin,rtmin is at the correct corner
-        featureGroup.position.set(-Graph.viewRange.mzmin*mz_squish, 0, Graph.gridRange - Graph.viewRange.rtmin*rt_squish);
-    }*/
     static loadMzrtData = (minmz, maxmz, minrt, maxrt) => {
         return new Promise((resolve, reject) => {
             if($('#featureStatus').val() != "0"){
@@ -138,8 +78,8 @@ class GraphFeature{
                 xhttp.onload = () => {
                     if (xhttp.readyState == 4 && xhttp.status == 200) {
                         let featureData = JSON.parse(xhttp.responseText);
-                        //GraphFeature.addFeatureToGraph(featureData, minmz, maxmz, minrt, maxrt);            
-                        GraphFeature.updateFeature(featureData, minmz, maxmz, minrt, maxrt);            
+                        Graph.currentFeatureData = featureData;
+                        GraphFeature.updateFeature(minmz, maxmz, minrt, maxrt);            
                         resolve();
                     } 
                 }
@@ -148,6 +88,14 @@ class GraphFeature{
         })
     } 
     static drawFeature = (viewRange) => {
+        return new Promise((resolve, reject) => {
+            let promise = GraphFeature.loadMzrtData(viewRange.mzmin, viewRange.mzmax, viewRange.rtmin/60, viewRange.rtmax/60);
+            promise.then(() => {
+                resolve();
+            })
+        })
+    }
+    static drawFeatureNoDataLoad = (viewRange) => {
         return new Promise((resolve, reject) => {
             let promise = GraphFeature.loadMzrtData(viewRange.mzmin, viewRange.mzmax, viewRange.rtmin/60, viewRange.rtmax/60);
             promise.then(() => {
