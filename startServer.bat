06891a754c492859ecee464e18dbc7361c528ecc
inkwon <inkwchoi@iu.edu>
2021-05-17 13:48:31 -0400

    fix #114 - when a user tries to start an already running server

--- startServer.bat
+++ startServer.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
+taskkill /f /t /fi "WINDOWTITLE eq TopMSV" > NUL
 init.vbs
 ECHO TopMSV
 ECHO Please wait.... Chrome browser will open when the server is ready. It may take a few minutes.
