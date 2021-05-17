e6b3e4f73b2e5e312039f72e17c7bc3193e50ca4
inkwon <inkwchoi@iu.edu>
2021-05-17 13:30:12 -0400

    fix #115 - batch script to install dependencies

--- startServer.bat
+++ startServer.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 init.vbs
 ECHO TopMSV
-ECHO Please wait.... Chrome browser will open when the server is ready. 
+ECHO Please wait.... Chrome browser will open when the server is ready. It may take a few minutes.
 pause
