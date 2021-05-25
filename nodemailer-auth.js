c9d686079cb46a9912f28653f259494a4f69dd62
inkwon <inkwchoi@iu.edu>
2021-05-25 14:22:01 -0400

    fix #136

--- nodemailer-auth.js
+++ nodemailer-auth.js
@@ -1,24 +1,42 @@
 const nodemailer = require('nodemailer');
+const fs = require('fs');
 
-const transport = nodemailer.createTransport({
-    host: "HOSTNAME", // hostname
-    secureConnection: false, // TLS requires secureConnection to be false
-    port: 587, // port for secure SMTP
-    tls: {
-        ciphers:'SSLv3'
-    },
-    auth: {
-        user: 'USERNAME',
-        pass: 'PW'
+let sendEmail = true;
+let transport = null;
+let message = null;
+	
+if (fs.existsSync('config.json')) {
+    let configData = fs.readFileSync('config.json');
+    configData = JSON.parse(configData);
+    if (!configData.sendEmail) {
+		sendEmail = false;
     }
-});
+}
 
-const message = {
-    from: 'YOUR_EMAIL',
-    to: 'default@gmail.com',         // List of recipients
-    subject: 'Default Subject', // Subject line
-    text: 'Default text' // Plain text body
-};
+if (sendEmail) {
+	transport = nodemailer.createTransport({
+		host: HOSTNAME, // hostname
+		secureConnection: false, // TLS requires secureConnection to be false
+		port: 587, // port for secure SMTP
+		tls: {
+			ciphers:'SSLv3'
+		},
+		auth: {
+			user: USERNAME,
+			pass: PW
+		}
+	});
+	message = {
+		from: YOUR_EMAIL,
+		to: 'default@gmail.com',         // List of recipients
+		subject: 'Default Subject', // Subject line
+		text: 'Default text' // Plain text body
+	};
+}
 
 module.exports.transport = transport;
 module.exports.message = message;
+
+
+
+
