85655d9ab1ba600f770a5a51a7a0d484abb79a3a
inkwon <inkwchoi@iu.edu>
2021-05-25 14:22:01 -0400

    fix #136 & added -y to python package install

--- installServer.sh
+++ installServer.sh
@@ -4,7 +4,7 @@ sudo apt-get upgrade -y
 curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y nodejs build-essential
-sudo apt-get install python-is-python3 
+sudo apt-get install -y python-is-python3 
 npm install
 echo .
 echo . 
