c00537e453e9360fe712d9fd761f0d4de5b4c3a2
inkwon <inkwchoi@iu.edu>
2021-07-13 09:44:55 -0400

    fix #193 - skip redraw when rotating

--- public/js/3d_graph/graph_control/graph_render.js
+++ public/js/3d_graph/graph_control/graph_render.js
@@ -2,11 +2,12 @@ class GraphRender{
     constructor(){}
     static checkAndRender = () => {
         if (Graph.currentData.length > 0){
-            if (Graph.camera.position.y > 25.495){
+            if (Graph.camera.position.y > 25.495){//if switching to 2D view
                 Graph.isPerpendicular = true;
                 //if camera is perpendicular to view, switch to 2D
                 GraphData.plotPoint2D();
-            }else{
+            }
+            else if (Graph.isPerpendicular) {//if switching from 2D view to 3D view
                 Graph.isPerpendicular = false;
                 let dataGroup = Graph.scene.getObjectByName("dataGroup");
                 let prevGroup = dataGroup.getObjectByName("peak2DGroup");
@@ -15,6 +16,7 @@ class GraphRender{
                 //let curRT = document.getElementById("scan1RT").innerText;    
                 GraphData.draw(Graph.curRT);
             }
+            //do nothing if just rotating 3D view without switching to 2D
         }
         this.renderImmediate();
     }
