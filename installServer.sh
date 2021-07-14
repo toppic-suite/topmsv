989d6e5f8eec0396df74a199b3268655a3bbdd68
inkwon <inkwchoi@iu.edu>
2021-07-14 10:00:29 -0400

    add install scripts for node modules in subfolder

--- installServer.sh
+++ installServer.sh
@@ -4,6 +4,8 @@ sudo apt-get upgrade -y
 curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
 npm install
+cd public/resources/topview
+npm install
 echo .
 echo . 
 echo TopMSV installation finished. 
