0dc3013c90419edc4658408295a667149537bb85
inkwon <inkwchoi@iu.edu>
2021-07-22 14:36:56 -0500

    fix #199 - cannot sign out

--- public/js/home.js
+++ public/js/home.js
@@ -2,6 +2,7 @@ function getCookie(cname) {
     var name = cname + "=";
     var decodedCookie = decodeURIComponent(document.cookie);
     var ca = decodedCookie.split(';');
+    console.log("ca", ca);
     for(var i = 0; i <ca.length; i++) {
         var c = ca[i];
         while (c.charAt(0) == ' ') {
@@ -21,13 +22,16 @@ if(getCookie('token')===''){
 }
 function callAuthentication(e) {//when user clicks sign in button
     e.preventDefault();
-    let shouldAuthenticate = document.getElementById("auth-value").textContent;
-    if (shouldAuthenticate == "true") {
-        window.location = window.location.href + "auth/google";
-    }else if (shouldAuthenticate == "false") {
-        window.location = window.location.href + "auth/skip";
+    if(getCookie('token')===''){
+        let shouldAuthenticate = document.getElementById("auth-value").textContent;
+        if (shouldAuthenticate == "true") {
+            window.location = window.location.href + "auth/google";
+        }else if (shouldAuthenticate == "false") {
+            window.location = window.location.href + "auth/skip";
+        }
+    } else {
+        window.location = window.location.href + "logout";
     }
-
 }
 $(document).ready(() => {
     //check browser
