e4f388bd5d953a5dcc155e81a35bf7bf538e2d8d
239cda <inkwon.choi@gmail.com>
2021-11-09 09:18:26 -0600

    fix #226 & removed unused script file

--- installServer.sh
+++ installServer.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
+rm -r node_modules
 npm install
 cd public/resources/topview
 npm install
