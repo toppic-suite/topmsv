8e6bcd2d50904902d77d6c2b4842edabd6d69bae
Choi <ichoi2@tulane.edu>
2022-01-31 10:35:53 -0600

    removed Linux related files

---
+++ cpp/build/CMakeFiles/mzMLReader.dir/DependInfo.cmake
@@ -0,0 +1,58 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/ChoiInKwon/Documents/topmsv_server/windows_release_1.0.1/topmsv-win-1.0.1/cpp/src/sqlite/sqlite3.c" "CMakeFiles/mzMLReader.dir/src/sqlite/sqlite3.c.obj" "gcc" "CMakeFiles/mzMLReader.dir/src/sqlite/sqlite3.c.obj.d"
+  "C:/Users/ChoiInKwon/Documents/topmsv_server/windows_release_1.0.1/topmsv-win-1.0.1/cpp/src/main.cpp" "CMakeFiles/mzMLReader.dir/src/main.cpp.obj" "gcc" "CMakeFiles/mzMLReader.dir/src/main.cpp.obj.d"
+  "C:/Users/ChoiInKwon/Documents/topmsv_server/windows_release_1.0.1/topmsv-win-1.0.1/cpp/src/msReader.cpp" "CMakeFiles/mzMLReader.dir/src/msReader.cpp.obj" "gcc" "CMakeFiles/mzMLReader.dir/src/msReader.cpp.obj.d"
+  "C:/Users/ChoiInKwon/Documents/topmsv_server/windows_release_1.0.1/topmsv-win-1.0.1/cpp/src/mzMLReader.cpp" "CMakeFiles/mzMLReader.dir/src/mzMLReader.cpp.obj" "gcc" "CMakeFiles/mzMLReader.dir/src/mzMLReader.cpp.obj.d"
+  "C:/Users/ChoiInKwon/Documents/topmsv_server/windows_release_1.0.1/topmsv-win-1.0.1/cpp/src/pwiz/data/common/CVTranslator.cpp" "CMakeFiles/mzMLReader.dir/src/pwiz/data/common/CVTranslator.cpp.obj" "gcc" "CMakeFiles/mzMLReader.dir/src/pwiz/data/common/CVTranslator.cpp.obj.d"
+  "C:/Users/ChoiInKwon/Documents/topmsv_server/windows_release_1.0.1/topmsv-win-1.0.1/cpp/src/pwiz/data/common/ParamTypes.cpp" "CMakeFiles/mzMLReader.dir/src/pwiz/data/common/ParamTypes.cpp.obj" "gcc" "CMakeFiles/mzMLReader.dir/src/pwiz/data/common/ParamTypes.cpp.obj.d"
+  "C:/Users/ChoiInKwon/Documents/topmsv_server/windows_release_1.0.1/topmsv-win-1.0.1/cpp/src/pwiz/data/common/cv.cpp" "CMakeFiles/mzMLReader.dir/src/pwiz/data/common/cv.cpp.obj" "gcc" "CMakeFiles/mzMLReader.dir/src/pwiz/data/common/cv.cpp.obj.d"
+  "C:/Users/ChoiInKwon/Documents/topmsv_server/windows_release_1.0.1/topmsv-win-1.0.1/cpp/src/pwiz/data/common/diff_std.cpp" "CMakeFiles/mzMLReader.dir/src/pwiz/data/common/diff_std.cpp.obj" "gcc" "CMakeFiles/mzMLReader.dir/src/pwiz/data/common/diff_std.cpp.obj.d"
+  "C:/Users/ChoiInKwon/Documents/topmsv_server/windows_release_1.0.1/topmsv-win-1.0.1/cpp/src/pwiz/data/msdata/BinaryDataEncoder.cpp" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/BinaryDataEncoder.cpp.obj" "gcc" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/BinaryDataEncoder.cpp.obj.d"
+  "C:/Users/ChoiInKwon/Documents/topmsv_server/windows_release_1.0.1/topmsv-win-1.0.1/cpp/src/pwiz/data/msdata/ChromatogramList_mzML.cpp" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/ChromatogramList_mzML.cpp.obj" "gcc" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/ChromatogramList_mzML.cpp.obj.d"
+  "C:/Users/ChoiInKwon/Documents/topmsv_server/windows_release_1.0.1/topmsv-win-1.0.1/cpp/src/pwiz/data/msdata/DefaultReaderList.cpp" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/DefaultReaderList.cpp.obj" "gcc" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/DefaultReaderList.cpp.obj.d"
+  "C:/Users/ChoiInKwon/Documents/topmsv_server/windows_release_1.0.1/topmsv-win-1.0.1/cpp/src/pwiz/data/msdata/IO.cpp" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/IO.cpp.obj" "gcc" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/IO.cpp.obj.d"
+  "C:/Users/ChoiInKwon/Documents/topmsv_server/windows_release_1.0.1/topmsv-win-1.0.1/cpp/src/pwiz/data/msdata/Index_mzML.cpp" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/Index_mzML.cpp.obj" "gcc" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/Index_mzML.cpp.obj.d"
+  "C:/Users/ChoiInKwon/Documents/topmsv_server/windows_release_1.0.1/topmsv-win-1.0.1/cpp/src/pwiz/data/msdata/LegacyAdapter.cpp" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/LegacyAdapter.cpp.obj" "gcc" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/LegacyAdapter.cpp.obj.d"
+  "C:/Users/ChoiInKwon/Documents/topmsv_server/windows_release_1.0.1/topmsv-win-1.0.1/cpp/src/pwiz/data/msdata/MSData.cpp" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/MSData.cpp.obj" "gcc" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/MSData.cpp.obj.d"
+  "C:/Users/ChoiInKwon/Documents/topmsv_server/windows_release_1.0.1/topmsv-win-1.0.1/cpp/src/pwiz/data/msdata/MSDataFile.cpp" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/MSDataFile.cpp.obj" "gcc" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/MSDataFile.cpp.obj.d"
+  "C:/Users/ChoiInKwon/Documents/topmsv_server/windows_release_1.0.1/topmsv-win-1.0.1/cpp/src/pwiz/data/msdata/MSNumpress.cpp" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/MSNumpress.cpp.obj" "gcc" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/MSNumpress.cpp.obj.d"
+  "C:/Users/ChoiInKwon/Documents/topmsv_server/windows_release_1.0.1/topmsv-win-1.0.1/cpp/src/pwiz/data/msdata/Reader.cpp" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/Reader.cpp.obj" "gcc" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/Reader.cpp.obj.d"
+  "C:/Users/ChoiInKwon/Documents/topmsv_server/windows_release_1.0.1/topmsv-win-1.0.1/cpp/src/pwiz/data/msdata/References.cpp" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/References.cpp.obj" "gcc" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/References.cpp.obj.d"
+  "C:/Users/ChoiInKwon/Documents/topmsv_server/windows_release_1.0.1/topmsv-win-1.0.1/cpp/src/pwiz/data/msdata/Serializer_MGF.cpp" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/Serializer_MGF.cpp.obj" "gcc" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/Serializer_MGF.cpp.obj.d"
+  "C:/Users/ChoiInKwon/Documents/topmsv_server/windows_release_1.0.1/topmsv-win-1.0.1/cpp/src/pwiz/data/msdata/Serializer_MSn.cpp" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/Serializer_MSn.cpp.obj" "gcc" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/Serializer_MSn.cpp.obj.d"
+  "C:/Users/ChoiInKwon/Documents/topmsv_server/windows_release_1.0.1/topmsv-win-1.0.1/cpp/src/pwiz/data/msdata/Serializer_mzML.cpp" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/Serializer_mzML.cpp.obj" "gcc" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/Serializer_mzML.cpp.obj.d"
+  "C:/Users/ChoiInKwon/Documents/topmsv_server/windows_release_1.0.1/topmsv-win-1.0.1/cpp/src/pwiz/data/msdata/Serializer_mzXML.cpp" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/Serializer_mzXML.cpp.obj" "gcc" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/Serializer_mzXML.cpp.obj.d"
+  "C:/Users/ChoiInKwon/Documents/topmsv_server/windows_release_1.0.1/topmsv-win-1.0.1/cpp/src/pwiz/data/msdata/SpectrumInfo.cpp" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/SpectrumInfo.cpp.obj" "gcc" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/SpectrumInfo.cpp.obj.d"
+  "C:/Users/ChoiInKwon/Documents/topmsv_server/windows_release_1.0.1/topmsv-win-1.0.1/cpp/src/pwiz/data/msdata/SpectrumList_BTDX.cpp" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/SpectrumList_BTDX.cpp.obj" "gcc" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/SpectrumList_BTDX.cpp.obj.d"
+  "C:/Users/ChoiInKwon/Documents/topmsv_server/windows_release_1.0.1/topmsv-win-1.0.1/cpp/src/pwiz/data/msdata/SpectrumList_MGF.cpp" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/SpectrumList_MGF.cpp.obj" "gcc" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/SpectrumList_MGF.cpp.obj.d"
+  "C:/Users/ChoiInKwon/Documents/topmsv_server/windows_release_1.0.1/topmsv-win-1.0.1/cpp/src/pwiz/data/msdata/SpectrumList_MSn.cpp" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/SpectrumList_MSn.cpp.obj" "gcc" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/SpectrumList_MSn.cpp.obj.d"
+  "C:/Users/ChoiInKwon/Documents/topmsv_server/windows_release_1.0.1/topmsv-win-1.0.1/cpp/src/pwiz/data/msdata/SpectrumList_mzML.cpp" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/SpectrumList_mzML.cpp.obj" "gcc" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/SpectrumList_mzML.cpp.obj.d"
+  "C:/Users/ChoiInKwon/Documents/topmsv_server/windows_release_1.0.1/topmsv-win-1.0.1/cpp/src/pwiz/data/msdata/SpectrumList_mzXML.cpp" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/SpectrumList_mzXML.cpp.obj" "gcc" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/SpectrumList_mzXML.cpp.obj.d"
+  "C:/Users/ChoiInKwon/Documents/topmsv_server/windows_release_1.0.1/topmsv-win-1.0.1/cpp/src/pwiz/data/msdata/SpectrumWorkerThreads.cpp" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/SpectrumWorkerThreads.cpp.obj" "gcc" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/SpectrumWorkerThreads.cpp.obj.d"
+  "C:/Users/ChoiInKwon/Documents/topmsv_server/windows_release_1.0.1/topmsv-win-1.0.1/cpp/src/pwiz/data/msdata/Version.cpp" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/Version.cpp.obj" "gcc" "CMakeFiles/mzMLReader.dir/src/pwiz/data/msdata/Version.cpp.obj.d"
+  "C:/Users/ChoiInKwon/Documents/topmsv_server/windows_release_1.0.1/topmsv-win-1.0.1/cpp/src/pwiz/utility/minimxml/SAXParser.cpp" "CMakeFiles/mzMLReader.dir/src/pwiz/utility/minimxml/SAXParser.cpp.obj" "gcc" "CMakeFiles/mzMLReader.dir/src/pwiz/utility/minimxml/SAXParser.cpp.obj.d"
+  "C:/Users/ChoiInKwon/Documents/topmsv_server/windows_release_1.0.1/topmsv-win-1.0.1/cpp/src/pwiz/utility/minimxml/XMLWriter.cpp" "CMakeFiles/mzMLReader.dir/src/pwiz/utility/minimxml/XMLWriter.cpp.obj" "gcc" "CMakeFiles/mzMLReader.dir/src/pwiz/utility/minimxml/XMLWriter.cpp.obj.d"
+  "C:/Users/ChoiInKwon/Documents/topmsv_server/windows_release_1.0.1/topmsv-win-1.0.1/cpp/src/pwiz/utility/misc/Base64.cpp" "CMakeFiles/mzMLReader.dir/src/pwiz/utility/misc/Base64.cpp.obj" "gcc" "CMakeFiles/mzMLReader.dir/src/pwiz/utility/misc/Base64.cpp.obj.d"
+  "C:/Users/ChoiInKwon/Documents/topmsv_server/windows_release_1.0.1/topmsv-win-1.0.1/cpp/src/pwiz/utility/misc/Filesystem.cpp" "CMakeFiles/mzMLReader.dir/src/pwiz/utility/misc/Filesystem.cpp.obj" "gcc" "CMakeFiles/mzMLReader.dir/src/pwiz/utility/misc/Filesystem.cpp.obj.d"
+  "C:/Users/ChoiInKwon/Documents/topmsv_server/windows_release_1.0.1/topmsv-win-1.0.1/cpp/src/pwiz/utility/misc/IntegerSet.cpp" "CMakeFiles/mzMLReader.dir/src/pwiz/utility/misc/IntegerSet.cpp.obj" "gcc" "CMakeFiles/mzMLReader.dir/src/pwiz/utility/misc/IntegerSet.cpp.obj.d"
+  "C:/Users/ChoiInKwon/Documents/topmsv_server/windows_release_1.0.1/topmsv-win-1.0.1/cpp/src/pwiz/utility/misc/IterationListener.cpp" "CMakeFiles/mzMLReader.dir/src/pwiz/utility/misc/IterationListener.cpp.obj" "gcc" "CMakeFiles/mzMLReader.dir/src/pwiz/utility/misc/IterationListener.cpp.obj.d"
+  "C:/Users/ChoiInKwon/Documents/topmsv_server/windows_release_1.0.1/topmsv-win-1.0.1/cpp/src/pwiz/utility/misc/SHA1.cpp" "CMakeFiles/mzMLReader.dir/src/pwiz/utility/misc/SHA1.cpp.obj" "gcc" "CMakeFiles/mzMLReader.dir/src/pwiz/utility/misc/SHA1.cpp.obj.d"
+  "C:/Users/ChoiInKwon/Documents/topmsv_server/windows_release_1.0.1/topmsv-win-1.0.1/cpp/src/pwiz/utility/misc/SHA1Calculator.cpp" "CMakeFiles/mzMLReader.dir/src/pwiz/utility/misc/SHA1Calculator.cpp.obj" "gcc" "CMakeFiles/mzMLReader.dir/src/pwiz/utility/misc/SHA1Calculator.cpp.obj.d"
+  "C:/Users/ChoiInKwon/Documents/topmsv_server/windows_release_1.0.1/topmsv-win-1.0.1/cpp/src/pwiz/utility/misc/random_access_compressed_ifstream.cpp" "CMakeFiles/mzMLReader.dir/src/pwiz/utility/misc/random_access_compressed_ifstream.cpp.obj" "gcc" "CMakeFiles/mzMLReader.dir/src/pwiz/utility/misc/random_access_compressed_ifstream.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cpp/build/CMakeFiles/mzMLReader.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for mzMLReader.
+# This may be replaced when dependencies are built.
