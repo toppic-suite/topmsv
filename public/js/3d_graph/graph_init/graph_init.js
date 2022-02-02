295d0aa35a364837698c32a764feb1aca600a2b4
239cda <inkwon.choi@gmail.com>
2022-02-02 10:19:55 -0600

    fix #300 - handle when intensity cutoff is empty

--- public/js/3d_graph/graph_init/graph_init.js
+++ public/js/3d_graph/graph_init/graph_init.js
@@ -28,6 +28,7 @@ class GraphInit{
             let rangeRT = parseFloat(document.getElementById('rtRangeMax').value);
             let centerMZ = parseFloat(document.getElementById('mzRangeMin').value);
             let rangeMZ = parseFloat(document.getElementById('mzRangeMax').value);
+            let inteCutoff = document.getElementById("cutoff-threshold").value;
 
             let minRT = Graph.viewRange.rtmin;
             let maxRT = Graph.viewRange.rtmax;
@@ -47,6 +48,14 @@ class GraphInit{
             if (!isNaN(rangeMZ)) {
                 minMZ = centerMZ - rangeMZ;
                 maxMZ = centerMZ + rangeMZ;
+            }
+
+            if (isNaN(inteCutoff) || inteCutoff == '') {
+                if (isNaN(parseFloat(inteCutoff))) {
+                    document.getElementById("cutoff-threshold").value = 0;
+                } else {
+                    document.getElementById("cutoff-threshold").value = parseFloat(inteCutoff);
+                }
             }
             //error handing
             if (minRT > maxRT){
