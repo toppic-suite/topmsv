5ed22e0dcfe440cdc0c91cf0113c71e1dd02cac1
239cda <inkwon.choi@gmail.com>
2022-02-07 08:32:58 -0600

    fix #277 fix #318 - allow upload of *_prsm.tsv

--- router/ptmQuery.js
+++ router/ptmQuery.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const getProjectSummary = require("../library/getProjectSummary");
 const parsePtm = require("../library/parsePtm");
+const fs = require('fs');
 
 /**
  * Express router for /ptmQuery
@@ -20,6 +21,17 @@ const ptmQuery = router.get('/ptmQuery', function (req, res) {
         let filePath = row.projectDir;
         let idx = filePath.indexOf(".mzML");
         filePath = (filePath.slice(0, idx)).concat("_ms2_toppic_prsm_single.tsv");
+
+        //doesn't really matter which file is used because the output is going to be the same
+        try {
+            if (!fs.existsSync(filePath)) {
+                filePath = (filePath.slice(0, idx)).concat("_ms2_toppic_prsm.tsv");
+            }
+        } catch(err) {
+            console.error(err);
+            console.error(err.stack);
+        }
+
         parsePtm(filePath, function(err, data){
             if (err) {
                 res.write(err);
