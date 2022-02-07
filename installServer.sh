96ffd3249311a25d2ea211dd767ce4804cc87b7f
Choi <ichoi2@tulane.edu>
2022-02-07 14:18:05 -0600

    fix #317 - removed nested node_modules

--- installServer.sh
+++ installServer.sh
@@ -3,8 +3,6 @@ curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
 mv node_modules node_modules_win
 npm install
-cd public/resources/topview
-npm install
 echo .
 echo . 
 echo TopMSV installation finished. 
