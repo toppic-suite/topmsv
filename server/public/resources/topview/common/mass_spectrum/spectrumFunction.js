00b1548263b1066f190c169634d4ed6624e9237f
239cda <inkwon.choi@gmail.com>
2022-03-10 11:24:42 -0600

    fix #383 - missing peak in mono mass view

--- server/public/resources/topview/common/mass_spectrum/spectrumFunction.js
+++ server/public/resources/topview/common/mass_spectrum/spectrumFunction.js
@@ -36,19 +36,27 @@ class SpectrumFunction {
             //for each peak, check with each interval level to see if the peak is the maximum intensity in the range
             for (let k = 0; k < this.mzLevel.length; k++) {
                 let eachInterval = this.mzLevel[k];
+                let curPeakLevel = k + 1; //[1-7]
                 let idx = Math.floor(mz / eachInterval.interval);
                 if (idx <= eachInterval.intervalNum) {
                     if (inte > eachInterval.maxInte) {
+                        if (eachInterval.maxPeakIdx >= 0) {
+                            let prevMaxPeakLevel = peaks[eachInterval.maxPeakIdx].getDisplayLevel();
+                            if (prevMaxPeakLevel >= curPeakLevel) {
+                                peaks[eachInterval.maxPeakIdx].setDisplayLevel(curPeakLevel - 1); //bring the down a level of prev. max peak in this range
+                            }
+                        }
                         eachInterval.maxInte = inte;
                         eachInterval.maxPeakIdx = i;
+                        peaks[i].setDisplayLevel(curPeakLevel);
                     }
                 }
                 else {
                     //update level of the highest peak in the currnet interval
                     //and increment the intervalNum (because this envelope belongs to next interval)
-                    if (peaks[eachInterval.maxPeakIdx].getDisplayLevel() < k + 1) { //k + 1 because assigned level should be between 1-7 (0 by default)
+                    if (peaks[i].getDisplayLevel() < curPeakLevel) {
                         //don't update the peak level if it is already assigned a larger level (= if it is a max peak in larger range) 
-                        peaks[eachInterval.maxPeakIdx].setDisplayLevel(k + 1);
+                        peaks[i].setDisplayLevel(curPeakLevel);
                     }
                     eachInterval.maxInte = inte;
                     eachInterval.maxPeakIdx = i;
@@ -81,9 +89,16 @@ class SpectrumFunction {
             envs[i].setDisplayLevel(0); //as an initial value
             for (let k = 0; k < this.mzLevel.length; k++) {
                 let eachInterval = this.mzLevel[k];
+                let curEnvLevel = k + 1; //[1-7]
                 let idx = Math.floor(mz / eachInterval.interval);
                 if (idx <= eachInterval.intervalNum) {
                     if (inte > eachInterval.maxInte) {
+                        if (eachInterval.maxPeakIdx >= 0) {
+                            let prevMaxEnvLevel = envs[eachInterval.maxPeakIdx].getDisplayLevel();
+                            if (prevMaxEnvLevel >= curEnvLevel) {
+                                envs[eachInterval.maxPeakIdx].setDisplayLevel(curEnvLevel - 1); //bring the down a level of prev. max peak in this range
+                            }
+                        }
                         eachInterval.maxInte = inte;
                         eachInterval.maxPeakIdx = i;
                     }
@@ -92,9 +107,9 @@ class SpectrumFunction {
                     //update level of the highest peak in the currnet interval
                     //and increment the intervalNum (because this envelope belongs to next interval)
                     if (eachInterval.maxPeakIdx >= 0) {
-                        if (envs[eachInterval.maxPeakIdx].getDisplayLevel() < k + 1) { //k + 1 because assigned level should be between 1-7 (0 by default)
+                        if (envs[i].getDisplayLevel() < k + 1) { //k + 1 because assigned level should be between 1-7 (0 by default)
                             //don't update the peak level if it is already assigned a larger level (= if it is a max peak in larger range) 
-                            envs[eachInterval.maxPeakIdx].setDisplayLevel(k + 1);
+                            envs[i].setDisplayLevel(k + 1);
                         }
                     }
                     else { //if it is the first envelope, there is no previous interval
