48ef376f09271e31cedf338a62391ac0deac39ac
239cda <inkwon.choi@gmail.com>
2022-03-11 09:42:03 -0600

    delete duplicate files

--- init.vbs
+++
@@ -1,4 +0,0 @@
-Set WshShell = CreateObject("WScript.Shell") 
-strCurDir    = WshShell.CurrentDirectory
-WshShell.Run chr(34) & strCurDir & "\init.bat" & Chr(34), 0
-Set WshShell = Nothing
--- installServer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-sudo apt-get install -y nodejs
-npm install
-echo .
-echo . 
-echo TopMSV installation finished. 
-echo Run startServer.sh to start TopMSV. 
